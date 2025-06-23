6607db55608f1ae38a145e3a19427e9dc23a4268
Charankumar <charankumaradhi@gmail.com>
2025-06-23 12:23:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+// Store rooms and messages
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+  });
+
+  socket.on("send_message", ({ room, encryptedMessage }) => {
+    console.log(`Encrypted message to ${room}:`, encryptedMessage);
+    socket.to(room).emit("receive_message", encryptedMessage);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
